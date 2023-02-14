d17682bccf315cac5475ee28dffe9d49c201de2e
nir-g0 <ngub24@gmail.com>
2023-02-14 08:41:16 +0000

    Assignment Commit

--- assignment1/countnames.c
+++ assignment1/countnames.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int* argc, char **argv){
+
 	char names[100][30];
+	int times[100];
+	
 	FILE *fp;
+	
 	fp = fopen(argv[1],"r");
+	
 	if(fp == NULL) {
      	  perror("Error opening file");
     	  return(-1);
@@ -13,7 +19,13 @@ int main(int* argc, char **argv){
    	  fgets(name, 30, fp);
    	  int complete = feof(fp);
    	  while(!complete){
-   	  	printf("%s",name);
+   	  	int len = strlen(name);
+   	  	if(len == 1){
+   	  		printf("\nEmpty Line\n\n");
+   	  	}
+   	  	else{
+   	  	   	printf("%s",name);
+   	  	}
    	  	fgets(name, 30, fp);
    	  	complete = feof(fp);
    	  }
