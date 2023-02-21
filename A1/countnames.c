25606edbfb9c5d316da1b1aba2cdf65ec10a8858
nir-g0 <nir-g0>
2023-02-21 05:30:14 +0000

    Test commit

--- A1/countnames.c
+++ A1/countnames.c
@@ -10,26 +10,34 @@ int main(int argc, char **argv){
 	
 	fp = fopen(argv[1],"r");
 	
+	//Checks for error in opening file
 	if(fp == NULL) {
      	  perror("Error opening file");
     	  return(-1);
    	}
+   	//Iterates through the list of names
    	else{
    	  char name[30];
    	  fgets(name, 30, fp);
    	  int complete = feof(fp);
    	  int line = 1;
+   	  //Iterates until reached end of file
    	  while(!complete){
    	  	int len = strlen(name);
+   	  	//empty line catch
    	  	if(len == 1){
 			fprintf( stderr, "Warning - Line %d is empty\n", line);
    	  	}
+   	  	//search name function
    	  	else{
-   	  		for(int i = 0; i < 100; i++){
+   	  		for(int i = 0; i < 100; i++){ 
+   	  			/*if the name is in the list of names, add one to the amount of 		
+   	  			times it appears*/
    	  			if(strcmp(names[i], name) == 0){
    	  				times[i] = times[i]+1;
    	  				break;
    	  			}
+   	  			//if the name is not in the list of names, add name to the list
    	  			if(strlen(names[i]) == 0){
    	  				for(int b = 0; b < 30; b++){
    	  					names[i][b] = name[b];
@@ -44,7 +52,7 @@ int main(int argc, char **argv){
    	  	line += 1;
    	  }
    	}
-	
+	//this loop prints the names and how many times they appear
 	for(int i = 0; i < 100; i++){
 		if(strlen(names[i]) != 0){
 		        names[i][strlen(names[i])-1] = '\0';
