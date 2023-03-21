723e29b2aa7a6f36bd8a981337449debafa9a132
nir-g0 <nir-g0>
2023-03-21 21:50:32 +0000

    A3 Commit

--- AS3/proc_manager.c
+++ AS3/proc_manager.c
@@ -13,13 +13,19 @@ int main(){
 	
 	int buffersize = 30;
 	char command[buffersize];
+	char message[MAX_LINE_LENGTH];
+	
 	int pid;
 	int argNum;
-	int outFileFD;
-	int errFileFD;
+	
+	int outFile;
+	int errFile;
+	
 	char stdoutFile[MAX_LINE_LENGTH];
 	char stderrFile[MAX_LINE_LENGTH];
 	int i = 1;
+	
+	
 	
 	while(fgets(command, buffersize , stdin)){
 	
@@ -29,37 +35,41 @@ int main(){
 		
 		if(pid == 0){
 		
-
-		
 			sprintf(stdoutFile, "%d.out", getpid());
 			sprintf(stderrFile, "%d.err", getpid());
 		
-			outFileFD = open(stdoutFile, O_RDWR | O_CREAT | O_APPEND, 0777);
-			errFileFD = open(stderrFile, O_RDWR | O_CREAT | O_APPEND, 0777);
+			outFile = open(stdoutFile, O_RDWR | O_CREAT | O_APPEND, 0777);
+			errFile = open(stderrFile, O_RDWR | O_CREAT | O_APPEND, 0777);
 			
-			
-			
-			if(dup2(outFileFD, 1) < 0){
+			if(dup2(outFile, STDOUT_FILENO) < 0){
 				printf("DUP2 FAILED");
 				exit(127);
 			}
-			if(dup2(errFileFD, 2) < 0){
+			if(dup2(errFile, STDERR_FILENO) < 0){
 				printf("DUP2 FAILED");
 				exit(127);
 			}
-			
-			char message[MAX_LINE_LENGTH];
+						
 			sprintf(message,"Starting command %d: child %d pid of parent %d\n", argNum, getpid(), getppid());
-			write(outFileFD, message, strlen(message));
+			write(outFile, message, strlen(message));
 			
 			exit(0);
 
-			
 		}
 		else{
 			while (wait ( NULL) != -1) {
+				
+				sprintf(stdoutFile, "%d.out", pid);
+				sprintf(stderrFile, "%d.err", pid);
+				
+				FILE *fp;
+				if((fp = fopen(stdoutFile, "a")) == NULL){
+					printf("FILE OPEN ERROR");
+				}
+				
+				sprintf(message,"Finished child %d pid of parent %d\n", pid, getpid());
+				fprintf(fp,message,strlen(message));
 
-				printf("Ending command %d\n", argNum);
 				i++;
 			}
 		}
